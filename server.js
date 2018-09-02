a8f0a818324e72962ae2c73e04088d7da743b8a9
Unknown <thibault.hardin@gmail.com>
2018-09-02 22:32:20 +0200

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/<crabe-dor>'));
 
 app.get('/*all', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
@@ -14,7 +14,7 @@ app.get('/*all', function(req, res) {
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/crabe-dor/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
